a53afda5746ffada2fda000e5041c084aeba1116
vineetsemwal <vineetsemwal82@gmail.com>
2022-03-25 15:23:29 +0530

    monolithic schoolms app with 2 modules

---
+++ java/jee/spring/boot/microservices/monolithic/schoolms/src/main/java/com/trainingapps/schoolms/studentms/dto/AddStudentInDepartmentRequest.java
@@ -0,0 +1,24 @@
+package com.trainingapps.schoolms.studentms.dto;
+
+public class AddStudentInDepartmentRequest {
+
+    private int studentId;
+
+    private int departmentId;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+}
